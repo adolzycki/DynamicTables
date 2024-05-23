0a799b6e3cd7da50dfbd5f01c4e1775dbb96ed71
Adam <a.dolzycki@gmail.com>
2024-05-23 22:05:07 +0200

    chore: init models

--- dynamic_tables/tables/models.py
+++ dynamic_tables/tables/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class DynamicModel(models.Model):
+    name = models.CharField(max_length=32, unique=True)
+
+
+class DynamicModelField(models.Model):
+    class DynamicModelFieldType(models.TextChoices):
+        STRING = "string", "String"
+        BOOLEAN = "boolean", "Boolean"
+        NUMBER = "number", "Number"
+
+    dynamic_model = models.ForeignKey(DynamicModel, on_delete=models.CASCADE, related_name="fields", default=None)
+    name = models.CharField(max_length=32, unique=True)
+    type = models.CharField(max_length=32, choices=DynamicModelFieldType.choices)
+    allow_blank = models.BooleanField(default=False)
+    allow_null = models.BooleanField(default=False)
