455b9823f4d376b5d4dc9f8bbc85e5b6176f22ff
Adam <a.dolzycki@gmail.com>
2024-05-23 23:34:32 +0200

    chore: remove comments

--- dynamic_tables/tables/admin.py
+++ dynamic_tables/tables/admin.py
@@ -4,15 +4,12 @@ from tables.models import DynamicModel, DynamicModelField
 
 class ReadOnlyAdmin(admin.ModelAdmin):
     def has_add_permission(self, request):
-        # Disable add permission
         return False
 
     def has_change_permission(self, request, obj=None):
-        # Allow viewing but not modifying
         return False if obj else True
 
     def has_delete_permission(self, request, obj=None):
-        # Disable delete permission
         return False
 
 
